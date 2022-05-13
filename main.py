751640c0c52d50622a7ac86e80860c48b1fab6ec
Omar Saad <62719848+Omar-Saad@users.noreply.github.com>
2022-05-13 16:38:04 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# LOW_RANGE =-100
+# HIGH_RANGE = 100
+from matrix import Matrix
+
+
+WELCOME_MSG = "Welcome to the Matrix Flatten Program!\n"
+def main():
+
+    print("Enter Matrix Dimensions: ")
+    flag = True
+    while flag:
+        try :
+            N = int(input('N = ').strip())
+            M = int(input('M = ').strip())
+            P = int(input('P = ').strip())
+            flag = False
+        except ValueError:
+            print("Invalid input. Please try again.")
+            flag = True
+            
+    print("\n")
+    matrix = Matrix(N,M,P)
+    matrix.flatten_matrix()
+
+    print("Matrix Created...")
+    show_matrix_q = input("Do you want to display the matrix? (y/n)")
+    if show_matrix_q == 'y':
+        print(matrix.matrix)
+    print("\n")
+
+    print("Matrix Flattened...")
+    matrix.flatten_matrix()
+    print("Matrix Flattened lenght = ", len(matrix.matrix_flattend))
+
+    show_matrix_q = input("Do you want to display the flattened matrix? (y/n)")
+    if show_matrix_q == 'y':
+        print(matrix.matrix_flattend)
+    print("\n")
+
+    
+    flag = True
+    while flag:
+
+        try:
+            i = int(input("Enter the index i: ").strip())
+            j = int(input("Enter the index j: ").strip())
+            k = int(input("Enter the index k: ").strip())
+    
+            index = matrix.get_1D_index_from_3D(i,j,k)
+
+            item =matrix.get_item_from_flatten_by_index(index)
+            print("Flattened matrix : The item at index {} is: {}".format(index, item))
+
+            item =matrix.get_item_from_3d_by_index(i,j,k)
+            print("Original matrix : The item at index {},{},{} is: {}".format(i,j,k, item))
+            flag = False
+        except IndexError:
+            print("Index out of range")
+            flag = True
+
+
+if __name__ == "__main__":
+    main()
---
+++ matrix.py
@@ -0,0 +1,31 @@
+from operator import le
+import numpy as np
+class Matrix:
+    def __init__(self,N,M,P):
+        self.N = N
+        self.M = M
+        self.P= P
+        self.matrix = self.generate_random_matrix(N,M,P)
+
+    def generate_random_matrix(self,N, M, P):
+        matrix = np.random.rand(N,M,P)
+        return matrix
+
+
+    def flatten_matrix(self):
+        self.matrix_flattend = []
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                for k in range(len(self.matrix[i][j])):
+                    self.matrix_flattend.append(self.matrix[i][j][k])
+
+        self.matrix_flattend
+
+    def get_item_from_flatten_by_index(self,index):
+        return self.matrix_flattend[index]
+
+    def get_item_from_3d_by_index(self,i,j,k):
+        return self.matrix[i][j][k]
+    
+    def get_1D_index_from_3D(self,i,j,k):
+        return i*self.M + j*self.P + k
